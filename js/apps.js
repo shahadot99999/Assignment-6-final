7e31a8d348dd3e9ac59fca3990861033a90d12a2
shahadot99999 <shahadot99999@gmail.com>
2022-11-19 19:21:35 +0600

    news categories card body set up and show display

--- js/apps.js
+++ js/apps.js
@@ -7,7 +7,7 @@ const loadNewsCategories = async () => {
 }
 
 //news category diplaybar
-const displayNewsCategories = async () =>{
+const displayOnlineNewsCategories = async () =>{
     try{
         const values = await loadNewsCategories();
         const categoriesList =document.getElementById('categories');
@@ -26,4 +26,80 @@ const displayNewsCategories = async () =>{
     catch(error){
         console.log(error);
     }
+}
+displayOnlineNewsCategories();
+
+//news categories card body
+
+const loadCategoriesId =async (category_id) =>{
+    toggleSpinner(true);
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    const num = await fetch(url);
+    const data = await num.json();
+    displayNewsCategoriesId( data.data);
+}
+
+const displayNewsCategoriesId = (datas) =>{
+    // input field
+    const inputField = document.getElementById('floatingInput');
+    inputField.value = datas.length + ' items found for category';
+
+    // data not found
+    const notFound = document.getElementById('not-found');
+    
+    //card area 
+    const cardGroups = document.getElementById('card-group');
+    cardGroups.innerHTML="";
+
+    // data sort 
+    
+    const sortArray = [];
+    // console.log(sortArray);
+    const dataa = datas.sort((a,b) =>{
+        return b.total_view -a.total_view;
+    });
+
+    if(datas.length>0){
+        for(const data of dataa){
+            const setDiv = document.createElement('div');
+            setDiv.classList.add('card');
+            setDiv.classList.add('mb-3');
+            setDiv.innerHTML = `
+            <div onclick="loadModal('${data._id}')" data-bs-toggle="modal" data-bs-target="#exampleModal" class="row g-0">
+            <div class="col-md-4">
+              <img src="${data.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${data.title}</h5>
+                <p class="card-text">${data.details.length>250?data.details.slice(0,250) + '....' : data.details}</p>
+              </div>
+              <div class="d-flex">
+               <div> 
+                    <a href="#"><img src="${data.author.img}" class="ms-lg-5 ms-sm-2 mt-2 border rounded-circle
+                      nav-img" alt="Cinque Terre"></a>
+                </div>
+                <div class="ps-2">
+                    <p>${data.author.name? data.author.name : 'no name'} <br> 
+                        ${data.author.published_date? data.author.published_date : 'no date'}</p>
+                </div>
+                <div class="p-lg-2 ps-lg-5 ps-5 mt-sm-3">
+                <h6><i class="fa-solid fa-eye"></i> ${data.total_view?data.total_view : 'no view'}</h6>
+              </div>
+                <div class="text-lg-end w-50 w-sm-25 ps-sm-5 ms-sm-5 mt-sm-3">
+                <a href="#"><i class="fa-sharp fa-solid fa-arrow-right"></i></a>
+                </div>
+                </div>
+            </div>
+          </div>
+            
+            `;
+            cardGroups.appendChild(setDiv); 
+        }
+        notFound.classList.add('d-none');
+    }
+    else{
+        notFound.classList.remove('d-none');
+    }
+    toggleSpinner(false);
 }
